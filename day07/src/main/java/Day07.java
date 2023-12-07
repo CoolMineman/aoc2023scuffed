1cf72688e22d6c98246f613b1c64a4e387bf0a8a
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2023-12-06 23:48:57 -0600

    only top quality here (tm)

---
+++ day07/src/main/java/Day07.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Day07 {
+    static String[] inputS;
+
+    public static void main(String[] args) throws Throwable {
+        try (BufferedReader r = Files.newBufferedReader(Paths.get("input.txt"))) {
+            inputS = r.lines().map(String::strip).toArray(String[]::new);
+        }
+
+        enum Type {
+            FIVE,
+            FOUR,
+            FULL,
+            THREE,
+            TWO,
+            ONE,
+            HIGH
+        }
+
+        record Game(String cards, int bid) implements Comparable<Game> {
+
+            Type type() {
+                HashMap<Character, Integer> charCnts = new HashMap<>();
+                for (char c : cards.toCharArray()) {
+                    charCnts.put(c, charCnts.getOrDefault(c, 0) + 1);
+                }
+                if (charCnts.size() == 1) {
+                    return Type.FIVE;
+                } else if (charCnts.size() == 2 && charCnts.entrySet().stream().anyMatch(i -> i.getValue() == 4)) {
+                    return Type.FOUR;
+                } else if (charCnts.size() == 2 && charCnts.entrySet().stream().anyMatch(i -> i.getValue() == 3)) {
+                    return Type.FULL;
+                } else if (charCnts.entrySet().stream().anyMatch(i -> i.getValue() == 3)) {
+                    return Type.THREE;
+                } else if (charCnts.entrySet().stream().filter(i -> i.getValue() == 2).toList().size() == 2) {
+                    return Type.TWO;
+                } else if (charCnts.entrySet().stream().anyMatch(i -> i.getValue() == 2)) {
+                    return Type.ONE;
+                } else {
+                    return Type.HIGH;
+                }
+            }
+
+            static List<Character> power = Arrays.asList('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J');
+
+            @Override
+            public int compareTo(Game o) {
+                int r = -type().compareTo(o.type());
+                if (r != 0) return r;
+                for (int i = 0; i < cards.length(); i++) {
+                    if (power.indexOf(cards.charAt(i)) > power.indexOf(o.cards.charAt(i))) {
+                        return -1;
+                    } else if (power.indexOf(cards.charAt(i)) < power.indexOf(o.cards.charAt(i))) {
+                        return 1;
+                    }
+                }
+                return 0;
+            }
+
+        };
+
+        List<Game> games = new ArrayList<>(List.of(inputS).stream().map(s -> new Game(s.split(" ")[0], Integer.parseInt(s.split(" ")[1]))).toList());
+        games.sort(Game::compareTo);
+        System.out.println(games);
+        int r = 0;
+        for (int i = 0; i < games.size(); i++) {
+            var g = games.get(i);
+            r += (i + 1) * g.bid();
+        }
+        System.out.println(r);
+    }
+}
